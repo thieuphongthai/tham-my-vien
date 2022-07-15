08f8f70d2abe2cca2052d4bedb7f7966a87a4552
thieuphongthai <thaithieu.phong119@gmail.com>
2022-07-16 01:17:20 +0700

    fix verify token and check department and position

--- src/routes/auth-route.js
+++ src/routes/auth-route.js
@@ -6,11 +6,9 @@ const authJwt = require('../middleware/authJwt');
 const controller = require("../app/controllers/AuthController");
 const rootRouter = require("./root");
 const adminRouter = require("./admin");
-const businessRouter = require("./departments/business");
 const marketingRouter = require("./departments/marketing");
 const receptionRouter = require("./departments/reception");
 const operatingRouter = require("./departments/operating-room");
-const saleRouter = require("./departments/sale");
 const hrRouter = require("./departments/human-resource");
 const BusinessController = require('../app/controllers/BusinessController');
 const validateUploadImage = require('../middleware/validateUploadImage');
@@ -18,19 +16,25 @@ const validateUploadImage = require('../middleware/validateUploadImage');
 router.get("/root", rootRouter);
 router.get("/admin", adminRouter);
 
+/* Business Employ Start*/
 router.get('/customers/:id/detail', BusinessController.getOneBusinessCustomer)
 router.post('/customers/:id/service-note', BusinessController.createServiceNote);
 router.get('/service-note', BusinessController.showServiceNote);
 router.patch('/customers/:id/comment', BusinessController.createComment)
 router.put('/customers/:id', validateUploadImage.upload, BusinessController.editCustomer);
 router.post('/customers', validateUploadImage.upload, BusinessController.createCustomer);
-router.get('/customers', BusinessController.showCustomer);
+router.get('/customers', [authJwt.verifyToken, authJwt.isBusinessEmploy], BusinessController.showCustomer);
+/* Business Employ End*/
 
 
-router.get("/marketing", [authJwt.verifyToken, authJwt.isUser], marketingRouter);
+/* Business Manager Start*/
+// Them route theo tung phong ban va chia theo chuc vu vao cap comment /*...*/
+/* Business Manager End*/
+
+
+router.get("/marketing", [authJwt.verifyToken, authJwt.isMarketingEmploy], marketingRouter);
 router.get("/reception", receptionRouter);
 router.get("/operating", operatingRouter);
-router.get("/sale", saleRouter);
 router.get("/hr", hrRouter);
 
 
